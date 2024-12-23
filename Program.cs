0b0b8ab23356741077dc0d8bb76542fc924a0121
Bug1642 <hasancan1642@hotmail.com>
2024-12-23 15:01:40 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ArithmeticExerciseOne {
+    public class Product {
+        public double newPayments(double cost, double deliveryFee) {
+           double cents = (cost + deliveryFee) * 100;
+            return cents;
+        }
+    }
+    class Program{
+        static void Main(string [] args ){
+            Product Total = new Product(); 
+
+            Console.WriteLine("Total in Cents: "+ Total.newPayments(19.995, 5.4925));
+        }
+    }
+}
